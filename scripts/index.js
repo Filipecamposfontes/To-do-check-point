5dee66e4d374c34bb507a70c398182b7330326eb
Filipecamposfontes <filipe.campos.fontes@gmail.com>
2022-03-29 20:56:00 -0300

    "Check point 2 projeto"

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+/*======================================================================= 
+Check point II - Aplicação To-Do
+Alunos: Filipe Campos | Harry Möbbs Júnior | Juan Barcelos | Fabiana Yumi Sato | Fernanda Brum
+GITS: @Filipecamposfontes | @harrymobbsjunior | @JuanBarcelos | FabianaYSK | @fernanda-brum
+
+Professor: IVIN Rodrigues 
+Turma: 09 Noite
+============================================================================= */
+
+
+document.querySelector('#submit-btn-login').addEventListener('click', (reload)=> {
+
+    /* Variaveis do formulario e captura de valores digitados */
+    
+  
+    reload.preventDefault()
+
+    let inputPasswordReferencia = document.querySelector("#inputPassword").value;
+    let inputEmailReferencia = document.querySelector('#inputEmail').value;
+
+    /* Validação E-mail */
+
+
+
+    /* Validação Senha */
+
+
+})
---
+++ scripts/utils.js
@@ -0,0 +1,8 @@
+/*======================================================================= 
+Check point II - Aplicação To-Do
+Alunos: Filipe Campos | Harry Möbbs Júnior | Juan Barcelos | Fabiana Yumi Sato | Fernanda Brum
+GITS: @Filipecamposfontes | @harrymobbsjunior | @JuanBarcelos | FabianaYSK | @fernanda-brum
+
+Professor: IVIN Rodrigues 
+Turma: 09 Noite
+============================================================================= */
