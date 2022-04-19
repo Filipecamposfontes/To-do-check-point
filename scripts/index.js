93f0e4d48815708be11ce00346cb8e7e58e6eeb6
Filipecamposfontes <filipe.campos.fontes@gmail.com>
2022-04-19 17:26:30 -0300

    Atualização api

---
+++ scripts/index.js
@@ -0,0 +1,70 @@
+/*======================================================================= 
+Check point II - Aplicação To-Do
+Alunos: Filipe Campos | Harry Möbbs Júnior | Juan Barcelos | Fabiana Yumi Sato | Fernanda Brum
+GITS: @Filipecamposfontes | @harrymobbsjunior | @JuanBarcelos | FabianaYSK | @fernanda-brum
+
+Professor: IVIN Rodrigues 
+Turma: 09 Noite
+
+Documentação API: https://ctd-todo-api.herokuapp.com/#/
+============================================================================= */
+let buttonloginRef = document.querySelector('#submit-btn-login');
+
+buttonloginRef.addEventListener('click', event => {
+    event.preventDefault();
+
+    let emailReference = document.querySelector('#inputEmail');
+    let passwordReference = document.querySelector('#inputPassword');
+    
+
+    let credentials = {
+        email: emailReference.value,
+        password: passwordReference.value,
+    };
+
+    let requestHeaders = {
+        'Content-Type': 'application/json'
+    };
+
+    let configRequest = {
+        method: 'POST',
+        body: JSON.stringify(credentials),
+        headers: requestHeaders
+    };
+
+     fetch('https://ctd-todo-api.herokuapp.com/v1/users/login',configRequest)
+     .then(response => {
+         response.json().then(
+             data => {
+                 localStorage.setItem('token', data.jwt);
+                 console.log(response.status)
+                 if(response.status === 201){
+                    window.location.href = './tarefas.html';
+                 };
+             }
+         );
+     });
+});
+
+
+
+/*
+document.querySelector('#submit-btn-login').addEventListener('click', reload => {
+
+    /* Variaveis do formulario e captura de valores digitados 
+    
+  
+    reload.preventDefault();
+    
+
+    if(!validaMail(email).isValid){
+        document.querySelector(".mensagemEmail").innerHTML = validaMail(email).message;
+        return;
+    }
+
+validaPassword(password)
+            
+
+
+})
+*/
---
+++ scripts/validacoes.js
@@ -0,0 +1,87 @@
+/*======================================================================= 
+Check point II - Aplicação To-Do
+Alunos: Filipe Campos | Harry Möbbs Júnior | Juan Barcelos | Fabiana Yumi Sato | Fernanda Brum
+GITS: @Filipecamposfontes | @harrymobbsjunior | @JuanBarcelos | FabianaYSK | @fernanda-brum
+
+Professor: IVIN Rodrigues 
+Turma: 09 Noite
+
+Documentação API: https://ctd-todo-api.herokuapp.com/#/
+
+============================================================================= */
+
+
+    /*=========================================================== 
+    Validação E-mail 
+    ==============================================================*/
+
+function validaMail (email){
+    if (!!email) {
+        let emailParsed = email.split('@');
+
+        if (emailParsed.length != 2) {
+            return { isValid: false, message: "email invalido" };
+        } else {
+            if (emailParsed[1].split('.').length != 2) {
+                return { isValid: false, message: "email invalido" };
+            }
+        }
+
+        return {isValid: true};
+    }else{
+        return { isValid: false, message: "O campo email nao pode estar vazio" };
+    }
+}
+
+
+    /* ===========================================
+    Validação Senha Entrada no sistema
+    ===============================================*/
+
+function validaPassword (password){
+
+     //Valida Senha em branco
+
+  if(password == "") {  
+    document.querySelector(".mensagem-password").innerHTML = "Campo senha vazio, por favor digite sua senha";  
+    return false;  
+ } 
+  
+else {  
+
+    /* Subistituir pela validação da API */
+
+    alert("Password correto");  
+ }  
+}  
+
+
+
+
+
+   /* ================================================
+   Validação Senha Novas Contas
+   ======================================================*/
+
+    function validaPasswordNovaConta (passwordNovaConta){
+
+        //Valida Senha Vazia
+   
+     if(passwordNovaConta == "") {  
+       document.querySelector(".mensagem-password").innerHTML = "A senha não pode ser vazia";  
+       return false;  
+    } 
+     
+   // Valida Senha menor que 8 
+   
+    if(passwordNovaConta.length < 8 || passwordNovaConta.length > 15) {  
+       document.querySelector(".mensagem-password").innerHTML = "A senha tem de ter no minimo 8 caracteres e ser menor que 15 caracteres";  
+       return false;  
+    } 
+    
+   else {  
+
+    // Substituir pelo post API
+       alert("Password is correct");
+    }
+}
